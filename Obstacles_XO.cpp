213ad98845a57bea9cd61fc9d1fc973c6c06e231
nadaahmed950 <ahmednanoo15@gmail.com>
2025-11-27 01:12:06 +0200

    adding win function to obstacles_XO

--- Obstacles_XO.cpp
+++ Obstacles_XO.cpp
@@ -29,13 +29,39 @@ bool Obstacles_XO::update_board(Move<char> *move) {
     return false;
 }
 
-bool Obstacles_XO::is_win(Player<char> *) {
+bool Obstacles_XO::is_win(Player<char> *player) {
+    const char sym = player->get_symbol();
+    auto all_equal = [&](char a, char b, char c , char d) {
+        return a == b && b == c && c==d && a != blank_symbol;
+    };
 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if ((all_equal(board[i][j], board[i][j+1], board[i][j+2],board[i][j+3]) && board[i][j] == sym) ||
+            (all_equal(board[i][j], board[i+1][j], board[i+2][j],board[i+3][j]) && board[i][j] == sym))
+                return true;
+        }
+    }
+    for (int i=0;i<3;i++) {
+        for (int j=0;j<3;j++) {
+            if (all_equal(board[i][j],board[i+1][j+1],board[i+2][j+2],board[i+3][j+3]) && board[i][j] == sym) {
+                return true;
+            }
+        }
+    }
+    for (int i=0;i<3;i++) {
+        for (int j=3;j<6;j++) {
+            if (all_equal(board[i][j],board[i+1][j-1],board[i+2][j-2],board[i+3][j-3]) && board[i][j] == sym) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 bool Obstacles_XO::is_lose(Player<char> *) {
 
 }
-bool Obstacles_XO::is_draw(Player<char> *) {
+bool Obstacles_XO::is_draw(Player<char> *player) {
     for (int i = 0; i < 6; i++) {
         for (int j = 0; j < 6; j++) {
             if (board[i][j] == '.')
