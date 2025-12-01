2a81b29ebdb989ae55d6b9a901f3cac163021473
mariam-mohamed225 <11410120240580@stud.cu.edu.eg>
2025-12-01 15:22:47 +0200

    Added Ultimate Files

--- Ultimate_XO.h
+++ Ultimate_XO.h
@@ -3,8 +3,25 @@
 #include "BoardGame_Classes.h"
 using namespace std;
 
-class Ultimate_XO {
+class Ultimate_XO : public Board<char> {
+private:
+    char blank_symbol = '.';
+public:
+    Ultimate_XO();
+    bool update_board(Move<char> *move);
+    bool is_win(Player<char>* player);
+    bool is_lose(Player<char>*);
+    bool is_draw(Player<char>* player);
+    bool game_is_over(Player<char>* player);
+};
 
+class Ultimate_XO_UI : public UI<char> {
+public:
+    Ultimate_XO_UI();
+    ~Ultimate_XO_UI();
+
+    Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+    Move<char>* get_move(Player<char>* player) override;
 };
 
 #endif //ULTIMATE_XO_H
