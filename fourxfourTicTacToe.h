b2a5de169b1a5f5e907db38ee3e9af56b5c61e7e
nadaahmed950 <ahmednanoo15@gmail.com>
2025-12-06 20:57:04 +0200

    update fourxfourTicTacToes

--- fourxfourTicTacToe.h
+++ fourxfourTicTacToe.h
@@ -11,9 +11,7 @@ using namespace std;
 
 class FourxFour_Board :public Board<char> {
 protected:
-
     void init_start_positions();
-
 
 public :
     FourxFour_Board();
@@ -21,7 +19,6 @@ public :
     char blank_symbol = '.';
     bool in_bounds(int r, int c) const;
     bool is_adjacent(int r, int c, int newr, int newc) const;
-
     bool update_board(Move<char>* move);
     bool is_win(Player<char>* player);
     bool is_lose(Player<char>*) { return false; };
@@ -29,38 +26,21 @@ public :
     bool game_is_over(Player<char>* player);
     vector<pair<int,int>> get_adjacent(int row, int col) ;
     bool checking_win(char player);
-    //FourxFour_Move* AImove( char ai, char player2);
-
     vector<FourxFour_Move*> get_all_valid_moves(char player_symbol);
     int evaluate1(char ai_symbol, char human_symbol);
-    // bool is_win_symbol(char sym);
-    //int minimax(char ai_symbol, char human_symbol, bool isMaximizingPlayer);
-    // في FourxFour_Board.h
-    //int minimax(char ai_symbol, char human_symbol, bool isMaximizingPlayer, int alpha, int beta);
     int minimax(char ai_symbol, char human_symbol, bool isMaximizingPlayer, int alpha, int beta, int depth) ;
     FourxFour_Move* best_move(char ai, char human);
-
     int evaluation2(char ai_symbol, char human_symbol);
     int count_alignment(char symbol, int length);
 };
 
 class FourxFour_UI : public UI<char> {
 public:
-    // FourxFour_UI(string message, int cell_width)
-    //     : UI<char>(message, cell_width) {}
     FourxFour_UI();
-
     ~FourxFour_UI();
 
     Player<char>* create_player(string& name, char symbol, PlayerType type);
     virtual Move<char>* get_move(Player<char>* player);
-    // vector<pair<int,int>> get_adjacent(int row, int col, vector<vector<char>>& board) const;
-    // bool checking_win(vector<vector<char>>& board, char player);
-    // void AImove(vector<vector<char>>& board, char ai, char player2);
-
-
-
-
 };
 
 class FourxFour_Move : public Move<char> {
